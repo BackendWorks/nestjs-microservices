e954f4b38aba230b46062f22d3baa5f64d6cb076
Harsh Makwana <harsh.make1998@gmail.com>
2022-02-19 16:52:50 +0530

    update install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ for entry in */; do
     if [ -e $entry/nest-cli.json ]; then
         echo "Installing `node_modules` in $entry..."
         cd $entry
-        npx husky-init && npm install
+        npm install
         cd ..
     fi
 done
