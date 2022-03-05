76636e466f0b5fa25a7339cc2e35532b37ae7048
Harsh Makwana <harsh.make1998@gmail.com>
2022-03-05 14:57:04 +0530

    wait for it added

--- run.sh
+++ run.sh
@@ -2,16 +2,16 @@
 echo "setting up server for env: $NODE_ENV"
 for entry in */; do
     if [ -e $entry/nest-cli.json ]; then
-        cp -r .env $entry.$NODE_ENV.env
+        cp -r .env $entry.env
     fi
 done
 
 if [ "$NODE_ENV" = "development" ]; then
-echo "starting docker-compose with file - docker-compose.dev.yml"
+    echo "starting docker-compose with file - docker-compose.dev.yml"
     docker-compose -f docker-compose.dev.yml up
 fi
 
 if [ "$NODE_ENV" = "production" ]; then
-echo "starting docker-compose with file - docker-compose.yml"
+    echo "starting docker-compose with file - docker-compose.yml"
     docker-compose -f docker-compose.yml up
 fi
