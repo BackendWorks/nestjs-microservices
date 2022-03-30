fdf9e09016e1add6da1aa1fc9d7916ec77d02601
Harsh Makwana <harsh.make1998@gmail.com>
2022-03-30 13:00:24 +0530

    docker wait-for-it changes

--- run.sh
+++ run.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
-echo "setting up server for env: $NODE_ENV"
+# remove slash from last echo ${@%/}
 for entry in */; do
     if [ -e $entry/nest-cli.json ]; then
         cp -r .env $entry.env
     fi
 done
-
-if [ "$NODE_ENV" = "development" ]; then
-    echo "starting docker-compose with file - docker-compose.yml"
-    docker-compose -f docker-compose.dev.yml up
-fi
-
-if [ "$NODE_ENV" = "production" ]; then
-    echo "starting docker-compose with file - docker-compose.prod.yml"
-    docker-compose -f docker-compose.yml up
-fi
+echo "starting docker-compose..."
+docker-compose up
