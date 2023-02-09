8f6e5c4cb10447acf40bb1ba31c67d7ed1a762af
Harsh <harsh.m@simformsolutions.com>
2023-02-10 02:21:30 +0530

    fix: deploy script ecr support

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+echo "Logging into AWS ecr"
+aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <ECR_URI>
 for entry in */; do
     if [ -e $entry/Dockerfile ]; then
         cd $entry
         echo "********** Deploying ${entry%/} ************"
         docker build -t nestjs-microservices-${entry%/}:latest -f Dockerfile .
         echo "********** Build completed ************"
-        docker tag nestjs-microservices-${entry%/}:latest hmake98/nestjs-microservices-${entry%/}:latest
+        docker tag nestjs-microservices-${entry%/}:latest <REPO_URI>:latest
         echo "********** Build tagged ************"
-        docker push hmake98/nestjs-microservices-${entry%/}:latest
+        docker push <REPO_URI>:latest
         echo "********** Build pushed ************"
         cd ..
     fi
