286c976f2916ea8258939d1c0baaf7ff2488a560
Harsh <harsh.m@simformsolutions.com>
2023-02-13 20:50:50 +0530

    fix: kong dockerfile, added deploy select options

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,24 @@
-#!/bin/sh
-echo "Logging into AWS ecr"
-aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <ECR_URI>
-for entry in */; do
-    if [ -e $entry/Dockerfile ]; then
-        cd $entry
-        echo "********** Deploying ${entry%/} ************"
-        docker build -t nestjs-microservices-${entry%/}:latest -f Dockerfile .
-        echo "********** Build completed ************"
-        docker tag nestjs-microservices-${entry%/}:latest <REPO_URI>:latest
-        echo "********** Build tagged ************"
-        docker push <REPO_URI>:latest
-        echo "********** Build pushed ************"
-        cd ..
-    fi
-done
+#!/bin/bash
 
+printf "Please select service:\n"
+select d in */; do
+    test -n "$d" && break
+    echo ">>> Invalid Selection"
+done
+if [ ${d%/} == 'fluent-bit' ]; then
+    exit 1
+fi
+if [ -e $d/Dockerfile ]; then
+    echo "Deploying service ${d%/}..."
+    cd $d
+    echo "Logging into AWS ecr"
+    aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <account-id>.dkr.ecr.ap-south-1.amazonaws.com/nestjs-microservices-${d%/}
+    echo "********** Deploying ${d%/} ************"
+    docker build -t nestjs-microservices-${d%/}:latest -f Dockerfile .
+    echo "********** Build completed ************"
+    docker tag nestjs-microservices-${d%/}:latest <account-id>.dkr.ecr.ap-south-1.amazonaws.com/nestjs-microservices-${d%/}:latest
+    echo "********** Build tagged ************"
+    docker push <account-id>.dkr.ecr.ap-south-1.amazonaws.com/nestjs-microservices-${d%/}:latest
+    echo "********** Build pushed ************"
+    cd ..
+fi
