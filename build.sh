0ecfa43b4890ceab6213a8b1aa3c6c013f8cc793
harsh-simform <harsh.m@simformsolutions.com>
2023-06-27 21:59:21 +0530

    fix: notification service values

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ for folder in */; do
     if [ -e $folder/Dockerfile ]; then
         echo ">>> Building service ${folder%/}..."
         cd $folder
-        docker build -t nestjs-microservices-${folder%/}:latest -f Dockerfile .
+        docker build --no-cache -t nestjs-microservices_${folder%/}:latest -f Dockerfile .
         echo ">>> Build completed"
         cd ..
     fi
