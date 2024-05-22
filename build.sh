8ba6540ebdbb6260d9bb29a466f4dcb173602612
Harsh Makwana <harsh.make1998@gmail.com>
2024-05-22 16:07:02 +0530

    update microservice pattern

--- build.sh
+++ build.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+# Function to build a Docker image for a given folder
 build_docker_image() {
     local folder="$1"
     local service_name=$(basename "$folder")
     if [[ -e "$folder/Dockerfile" ]]; then
         echo ">>> Building service ${service_name}..."
-        docker build -t "${service_name}:latest" -f "$folder/Dockerfile" "$folder" && echo ">>> Build completed for ${service_name}"
-        echo ">>> Built ${service_name}..."
+        if docker build -t "${service_name}:latest" -f "$folder/Dockerfile" "$folder"; then
+            echo ">>> Build completed for ${service_name}"
+        else
+            echo ">>> Build failed for ${service_name}" >&2
+        fi
     fi
 }
 
-find . -maxdepth 1 -type d | while read -r dir; do
-    if [[ "$dir" != "." ]]; then
-        build_docker_image "$dir"
-    fi
-done
+# Export the function to be used by parallel
+export -f build_docker_image
+
+# Find all directories with a Dockerfile and build images in parallel
+find . -maxdepth 1 -type d -not -path '.' -print0 | xargs -0 -n 1 -P $(nproc) bash -c 'build_docker_image "$0"'
 
 echo ">>> All builds completed"
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+# Function to log into AWS ECR
+aws_ecr_login() {
+    echo ">>> Logging into AWS ECR"
+    aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
+}
+
+# Function to build and push Docker image
+build_and_push_image() {
+    local service_name="$1"
+    local image_name="nestjs_$service_name:latest"
+    local ecr_image_uri="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$service_name:latest"
+
+    echo ">>> Building $image_name"
+    docker build -t "$image_name" -f "$service_name/Dockerfile" "$service_name"
+
+    if [ $? -eq 0 ]; then
+        echo ">>> Tagging $image_name"
+        docker tag "$image_name" "$ecr_image_uri"
+
+        echo ">>> Pushing $image_name"
+        docker push "$ecr_image_uri"
+
+        if [ $? -eq 0 ]; then
+            echo ">>> Deployment completed for $service_name"
+        else
+            echo ">>> Failed to push image $image_name" >&2
+        fi
+    else
+        echo ">>> Failed to build image $image_name" >&2
+    fi
+}
+
+# Main script execution
 printf "Please select service:\n"
 select d in */; do
     test -n "$d" && break
@@ -9,35 +42,14 @@ done
 service_name="${d%/}"
 
 if [ "$service_name" == 'fluent-bit' ]; then
+    echo ">>> Service fluent-bit is not allowed for deployment"
     exit 1
 fi
 
-service_dir="./$service_name"
-
-if [ ! -e "$service_dir/Dockerfile" ]; then
+if [ ! -e "$service_name/Dockerfile" ]; then
     echo ">>> Dockerfile not found for service $service_name"
     exit 1
 fi
 
 echo ">>> Deploying service $service_name..."
-cd "$service_dir" || exit
-
-aws_ecr_login() {
-    echo ">>> Logging into AWS ECR"
-    aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/nestjs-microservices-$service_name"
-}
-
-build_and_push_image() {
-    local image_name="nestjs_$service_name:latest"
-    echo ">>> Building $image_name"
-    docker build -t "$image_name" -f Dockerfile .
-    echo ">>> Tagging $image_name"
-    docker tag "$image_name" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$service_name:latest"
-    echo ">>> Pushing $image_name"
-    docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$service_name:latest"
-}
-
-aws_ecr_login && build_and_push_image
-
-echo ">>> Deployment completed for $service_name"
-cd ..
+aws_ecr_login && build_and_push_image "$service_name"
